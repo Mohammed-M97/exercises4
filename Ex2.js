db4492f6e29bb15a0e01d8ae0d8ce8af9d0e42a6
Mohammed-M97 <mohammed-alawlaqi@outlook.com>
2024-07-10 00:08:13 +0300

    add all changes

--- Ex2.js
+++ Ex2.js
@@ -1,10 +1,12 @@
 // Calculate average
 function findAverage(array) {
-  // your code here
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum / array.length;
+  if (array.length > 0) {
+      let sum = 0;
+      for (let i = 0; i < array.length; i++) {
+          sum += array[i];
+        }
+        return sum / array.length;
+    }
+    return 0
 }
-console.log(findAverage([1, 2, 3, 4]));
+console.log(findAverage([]));
